abfda0221f1c131e233a52133ff4740ed56486e2
John Emerson <john_emerson@tjxcanada.ca>
2022-10-24 12:45:56 -0600

    Print form data to console.

--- public/js/add-student.js
+++ public/js/add-student.js
@@ -3,3 +3,13 @@ import { linkLists } from './link-lists.js';
 let countriesSelect = document.getElementById('country');
 let provincesSelect = document.getElementById('province');
 linkLists(countriesSelect, provincesSelect);
+
+let data = document.getElementById('form');
+data.addEventListener('submit', function (event) {
+    event.preventDefault();
+    let formData = new FormData(data)
+
+    for (let entry of formData) {
+        console.log(`The ${entry[0]} field has the value ${entry[1]}.`);
+    }
+});
